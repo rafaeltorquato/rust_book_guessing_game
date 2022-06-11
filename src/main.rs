1e4c3c461edf5b2e4a615371b43f8fc3f413b08c
Rafael Torquato <rafaeltorquat0@protonmail.com>
2022-06-10 21:26:02 -0300

    ✨ Improved game with tries counter.

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,16 @@ use rand::Rng;
 
 fn main() {
     println!("Guesse the number!");
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is: {}", secret_number);
-
+    let mut tries_count = 0;
     loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
-            .expect("Failed to read line");
+            .expect("Failed to read line!");
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
@@ -26,13 +25,15 @@ fn main() {
             }
         };
 
-        println!("You guessed: {}", guess);
+        println!("You guessed: {}.", guess);
+        tries_count += 1;
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("{}", "Too small!".red()),
             Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Less => println!("{}", "Too small!".red()),
             Ordering::Equal => {
-                println!("{}", "You win!".green());
+                let msg = format!("You win with {} tries!", tries_count).green();
+                println!("{}", msg);
                 break;
             }
         };
